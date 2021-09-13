d96a6150cb6fedb947ff470f68e082fad9080275
Daniil <daniil@combinostics.com>
2021-09-13 13:18:44 +0300

    Fix memory leak caused by Query API, replace those with lambda expressions

--- src/Hangfire.LiteDB/LiteDbJobQueue.cs
+++ src/Hangfire.LiteDB/LiteDbJobQueue.cs
@@ -3,21 +3,17 @@ using System.Threading;
 using Hangfire.Annotations;
 using Hangfire.LiteDB.Entities;
 using Hangfire.Storage;
-using LiteDB;
 
 namespace Hangfire.LiteDB
 {
     /// <summary>
-    /// 
     /// </summary>
     public class LiteDbJobQueue : IPersistentJobQueue
     {
+        private readonly HangfireDbContext _connection;
         private readonly LiteDbStorageOptions _storageOptions;
 
-        private readonly HangfireDbContext _connection;
-
         /// <summary>
-        /// 
         /// </summary>
         /// <param name="connection"></param>
         /// <param name="storageOptions"></param>
@@ -28,7 +24,6 @@ namespace Hangfire.LiteDB
         }
 
         /// <summary>
-        /// 
         /// </summary>
         /// <param name="queues"></param>
         /// <param name="cancellationToken"></param>
@@ -36,36 +31,23 @@ namespace Hangfire.LiteDB
         [NotNull]
         public IFetchedJob Dequeue(string[] queues, CancellationToken cancellationToken)
         {
-            if (queues == null)
-            {
-                throw new ArgumentNullException(nameof(queues));
-            }
+            if (queues == null) throw new ArgumentNullException(nameof(queues));
 
-            if (queues.Length == 0)
-            {
-                throw new ArgumentException("Queue array must be non-empty.", nameof(queues));
-            }
-
-            var fetchConditions = new[]
-            {
-                Query.EQ("FetchedAt", null),
-                Query.LT("FetchedAt", DateTime.UtcNow.AddSeconds(_storageOptions.InvisibilityTimeout.Negate().TotalSeconds))
-            };
-            var fetchConditionsIndex = 0;
+            if (queues.Length == 0) throw new ArgumentException("Queue array must be non-empty.", nameof(queues));
 
             JobQueue fetchedJob = null;
             while (fetchedJob == null)
             {
                 cancellationToken.ThrowIfCancellationRequested();
 
-                var fetchCondition = fetchConditions[fetchConditionsIndex];
 
                 foreach (var queue in queues)
                 {
                     var lockQueue = string.Intern($"f13333e1-a0c8-48c8-bf8c-788e89030329_{queue}");
                     lock (lockQueue)
                     {
-                        fetchedJob = _connection.JobQueue.FindOne(Query.And(fetchCondition, Query.EQ("Queue", queue)));
+                        fetchedJob =
+                            _connection.JobQueue.FindOne(x => x.FetchedAt == null && x.Queue == queue);
 
                         if (fetchedJob != null)
                         {
@@ -76,22 +58,40 @@ namespace Hangfire.LiteDB
                     }
                 }
 
-                if (fetchedJob == null && fetchConditionsIndex == fetchConditions.Length - 1)
+                if (fetchedJob == null)
+                    foreach (var queue in queues)
+                    {
+                        var lockQueue = string.Intern($"f13333e1-a0c8-48c8-bf8c-788e89030329_{queue}");
+                        lock (lockQueue)
+                        {
+                            fetchedJob =
+                                _connection.JobQueue.FindOne(x =>
+                                    x.FetchedAt <
+                                    DateTime.UtcNow.AddSeconds(
+                                        _storageOptions.InvisibilityTimeout.Negate().TotalSeconds) && x.Queue == queue);
+
+                            if (fetchedJob != null)
+                            {
+                                fetchedJob.FetchedAt = DateTime.UtcNow;
+                                _connection.JobQueue.Update(fetchedJob);
+                                break;
+                            }
+                        }
+                    }
+
+                if (fetchedJob == null)
                 {
                     // ...and we are out of fetch conditions as well.
                     // Wait for a while before polling again.
                     cancellationToken.WaitHandle.WaitOne(_storageOptions.QueuePollInterval);
                     cancellationToken.ThrowIfCancellationRequested();
                 }
-
-                // Move on to next fetch condition
-                fetchConditionsIndex = (fetchConditionsIndex + 1) % fetchConditions.Length;
             }
 
             return new LiteDbFetchedJob(_connection, fetchedJob.Id, fetchedJob.JobId, fetchedJob.Queue);
         }
+
         /// <summary>
-        /// 
         /// </summary>
         /// <param name="queue"></param>
         /// <param name="jobId"></param>
